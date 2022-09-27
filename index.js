c0884aec0c43d08d1d8e63c006092beef270008b
mikeson <mikeson.jnr@gmail.com>
2022-09-27 12:10:02 +0100

    Add EventListener to start button

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
-const buttons = document.querySelectorAll("[data-avatarSwitch-button]");
 
-buttons.forEach(button =>{
-  button.addEventListener("click", () =>{
-    const offset = button.dataset.avatarswitchButton == "next" ? 1 : -1;
-    const slides = button.closest("[data-avatars]").querySelector("[data-slides]")
+//Helper Functions
+const helperFunction = (() => {
+  const switchPage = (currentPage, newPage) => {
+    currentPage.style.display = "none";
+    newPage.style.display = "flex";
+};
 
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+return {
+  switchPage
+  }
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
+})();
 
+const titlePage = (()=>{
+
+  //Avatar Selection (Arrow Buttons)
+  const buttons = document.querySelectorAll("[data-avatarSwitch-button]");
+  buttons.forEach(button =>{
+    button.addEventListener("click", () =>{
+      const offset = button.dataset.avatarswitchButton == "next" ? 1 : -1;
+      const slides = button.closest("[data-avatars]").querySelector("[data-slides]")
+  
+      const activeSlide = slides.querySelector("[data-active]");
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset
+      if (newIndex < 0) newIndex = slides.children.length - 1
+      if (newIndex >= slides.children.length) newIndex = 0
+  
+      slides.children[newIndex].dataset.active = true
+      delete activeSlide.dataset.active
+    })
   })
-})
+
+  //Start Button
+  const startBtn = document.querySelector("#start-btn")
+  startBtn.addEventListener("click", ()=>{
+    const titlePage = document.querySelector(".title-page");
+    const gameBoard = document.querySelector(".gameboard");
+    helperFunction.switchPage(titlePage, gameBoard);
+  })
+
+})();
