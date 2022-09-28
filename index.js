01bde15ccde14834339dc5ed83048f06cc4613f9
mikeson <mikeson.jnr@gmail.com>
2022-09-29 00:00:31 +0100

    Add EventListeners to cells to place markers

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-
 //Helper Functions
 const helperFunction = (() => {
   const switchPage = (currentPage, newPage) => {
     currentPage.style.display = "none";
     newPage.style.display = "flex";
-};
+  };
 
-return {
-  switchPage
+  return {
+    switchPage
   }
 
 })();
 
-const titlePage = (()=>{
+const titlePage = (() => {
 
   //Avatar Selection (Arrow Buttons)
   const switchButtons = document.querySelectorAll("[data-avatarSwitch-button]");
-  switchButtons.forEach(button =>{
-    button.addEventListener("click", () =>{
+  switchButtons.forEach(button => {
+    button.addEventListener("click", () => {
       const offset = button.dataset.avatarswitchButton == "next" ? 1 : -1;
       const slides = button.closest("[data-avatars]").querySelector("[data-slides]")
-  
+
       const activeSlide = slides.querySelector("[data-active]");
       let newIndex = [...slides.children].indexOf(activeSlide) + offset
       if (newIndex < 0) newIndex = slides.children.length - 1
       if (newIndex >= slides.children.length) newIndex = 0
-  
+
       slides.children[newIndex].dataset.active = true
       delete activeSlide.dataset.active
 
@@ -41,12 +40,12 @@ const titlePage = (()=>{
   //Player Name Input
   const playerOneInput = document.querySelector("#p1-name");
   playerOneInput.addEventListener("change", () => {
-      playerOneName.textContent += playerOneInput.value;
+    playerOneName.textContent += playerOneInput.value;
   });
 
   const playerTwoInput = document.querySelector("#p2-name");
   playerTwoInput.addEventListener("change", () => {
-      playerTwoName.textContent += playerTwoInput.value;
+    playerTwoName.textContent += playerTwoInput.value;
   });
 
   const playerOneName = document.querySelector("#player1-name");
@@ -56,11 +55,11 @@ const titlePage = (()=>{
 
   //Start Button
   const startBtn = document.querySelector("#start-btn")
-  startBtn.addEventListener("click", ()=>{
-    if(playerOneName.textContent == "" || playerOneName.textContent.trim().length == 0){
+  startBtn.addEventListener("click", () => {
+    if (playerOneName.textContent == "" || playerOneName.textContent.trim().length == 0) {
       playerOneName.textContent = "Player 1";
-     }
-    if(playerTwoName.textContent == "" || playerTwoName.textContent.trim().length == 0){
+    }
+    if (playerTwoName.textContent == "" || playerTwoName.textContent.trim().length == 0) {
       playerTwoName.textContent = "Player 2";
     }
     const titlePage = document.querySelector(".title-page");
@@ -72,7 +71,7 @@ const titlePage = (()=>{
 
 
 const gamePage = (() => {
-  
+
   // Player Factory Function
   const Player = (marker) => {
     return {
@@ -82,8 +81,45 @@ const gamePage = (() => {
 
   const playerOne = Player("X");
   const playerTwo = Player("O");
+  let playerOneTurn = true;
 
- 
+
+  const cellElements = document.querySelectorAll("[data-cell]");
+
+  cellElements.forEach(cell => {
+    cell.addEventListener("click", selectedCell, {
+      once: true
+    })
+  })
+
+  //Gets the selected cell
+  function selectedCell(event) {
+    const cell = event.target
+    if (playerOneTurn == true) {
+      placeMarker(cell, playerOne);
+      switchTurn();
+    } else if (playerOneTurn == false) {
+      placeMarker(cell, playerTwo);
+      switchTurn();
+    }
+  }
+
+  //Places Marker on selected Cell
+  function placeMarker(cell, player){
+    if (player == playerOne){
+    cell.textContent = "X";
+    } else if (player == playerTwo){
+      cell.textContent = "O";
+    }
+  }
+
+  //Switches the turn
+  function switchTurn(){
+    playerOneTurn = !playerOneTurn;
+    console.log(playerOneTurn);
+  }
+
+
 
 
 })();
